20dcea98d516d7ec4599cc0983957a17a7dc3d48
YuriLRodrigues <rodriguesyuri769@gmail.com>
2023-12-19 16:23:47 -0300

    feat: logo component

---
+++ src/components/interface/header/logo.tsx
@@ -0,0 +1,12 @@
+import Image from "next/image";
+import { ComponentProps } from "react";
+
+type LogoProps = ComponentProps<typeof Image>;
+
+export const Logo = ({ ...props }: LogoProps) => {
+  return (
+    <a href="#home">
+      <Image {...props} width={50} height={50} />
+    </a>
+  );
+};
