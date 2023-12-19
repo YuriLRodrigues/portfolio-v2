43e4f6140ce5d783952cb26d92c12de07731fcce
YuriLRodrigues <rodriguesyuri769@gmail.com>
2023-12-19 16:24:07 -0300

    feat: navlink component

---
+++ src/components/interface/header/navbar/nav-link.tsx
@@ -0,0 +1,15 @@
+import Link from "next/link";
+import { ComponentProps } from "react";
+
+type NavlinkProps = ComponentProps<"a">;
+
+export const Navlink = ({ ...props }: NavlinkProps) => {
+  return (
+    <li className="list-none cursor-pointer">
+      <a
+        className={`relative md:after:absolute md:after:content-[''] md:after:bg-gradient-to-r from-[#3a3172] from-67% to-[#a991f0] md:after:h-[2px] md:after:w-0 md:after:left-0 md:after:-bottom-[5px] md:hover:after:w-[100%] md:after:duration-500 pl-2 md:pl-0 uppercase font-medium text-[#fff] duration-500 md:border-l-0 border-l-2 border-[#3a3172] hover:text-[#aaaaaa]`}
+        {...props}
+      />
+    </li>
+  );
+};
