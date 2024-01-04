fd791efdad6a0d66db85fe940038fd99a42619f0
YuriLRodrigues <rodriguesyuri769@gmail.com>
2024-01-04 13:46:18 -0300

    feat: header component

--- src/components/interface/header/header.tsx
+++ src/components/interface/header/header.tsx
@@ -1,16 +1,16 @@
-import { Logo } from "./logo";
-import { MenuHamburger } from "./menu-hamburger";
-import { Navbar } from "./navbar/navbar";
-import { Navigation } from "./navbar/navgation";
+import { Logo } from './logo'
+import { MenuHamburger } from './menu-hamburger'
+import { Navbar } from './navbar/navbar'
+import { Navigation } from './navbar/navgation'
 
 export const Header = () => {
   return (
-    <header className="fixed flex justify-between bg-[#03001417]/50 items-center top-0 shadow-lg shadow-[#110b1f] w-full backdrop-blur-md z-50 md:px-10 px-1 py-2">
-      <Logo src={"/logo.png"} alt="logo-portfolio" />
+    <header className="fixed top-0 z-50 flex w-full items-center justify-between bg-[#220D3C]/100 px-1 py-2 backdrop-blur-md lg:bg-[#220D3C]/50 lg:px-10 lg:shadow-lg lg:shadow-[#110b1f]">
+      <Logo src={'/logo.png'} alt="logo-portfolio" />
       <MenuHamburger />
       <Navbar>
         <Navigation />
       </Navbar>
     </header>
-  );
-};
+  )
+}
