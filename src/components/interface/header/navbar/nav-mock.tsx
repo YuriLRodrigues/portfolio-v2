d6ec168ab4080e83d86c98f87bcca87843e94252
YuriLRodrigues <rodriguesyuri769@gmail.com>
2024-01-04 13:48:06 -0300

    feat: nav mock for each nav links

--- src/components/interface/header/navbar/nav-mock.tsx
+++ src/components/interface/header/navbar/nav-mock.tsx
@@ -1,22 +1,22 @@
 export const NavigationMock = [
   {
-    label: "início",
-    href: "#home",
-    current: "início",
+    label: 'início',
+    href: '#home',
+    current: 'início',
   },
   {
-    label: "sobre",
-    href: "#about",
-    current: "maps",
+    label: 'sobre',
+    href: '#about',
+    current: 'maps',
   },
   {
-    label: "habilidades",
-    href: "#skills",
-    current: "habilidades",
+    label: 'habilidades',
+    href: '#skills',
+    current: 'habilidades',
   },
   {
-    label: "projetos",
-    href: "#projects",
-    current: "projetos",
+    label: 'projetos',
+    href: '#projects',
+    current: 'projetos',
   },
-];
+]
