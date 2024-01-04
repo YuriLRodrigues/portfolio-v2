bc569295396677d353c8128a419c2368e1eff55e
YuriLRodrigues <rodriguesyuri769@gmail.com>
2024-01-04 13:44:31 -0300

    feat: card skills

---
+++ src/app/components/skills/card-skills.tsx
@@ -0,0 +1,30 @@
+'use client'
+import Tilt from 'react-parallax-tilt'
+
+type CardSkillsProps = {
+  time: number
+  children?: React.ReactNode
+}
+
+export const CardSkills = ({ time, children }: CardSkillsProps) => {
+  return (
+    <Tilt
+      perspective={500}
+      glareEnable={true}
+      glareMaxOpacity={0.45}
+      gyroscope={true}
+      glareBorderRadius="5px"
+      tiltReverse={true}
+    >
+      <div
+        className="group relative flex h-28 w-24 flex-col items-center justify-center overflow-hidden rounded-md bg-white/20 text-center backdrop-blur-md md:h-36 md:w-32"
+        data-aos="fade-right"
+        data-aos-duration="500"
+        data-aos-offset={`${time}`}
+        data-aos-easing="ease-in-sine"
+      >
+        {children}
+      </div>
+    </Tilt>
+  )
+}
