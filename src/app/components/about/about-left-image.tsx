a99c216a873480e844eee4f02aa81bd0cce80f4c
YuriLRodrigues <rodriguesyuri769@gmail.com>
2024-01-04 13:35:43 -0300

    feat: about left item

---
+++ src/app/components/about/about-left-image.tsx
@@ -0,0 +1,21 @@
+import Image from 'next/image'
+
+export const AboutLeftImage = () => {
+  return (
+    <div
+      className="mx-auto md:mt-10"
+      data-aos="fade-right"
+      data-aos-duration="500"
+      data-aos-offset="300"
+      data-aos-easing="ease-in-sine"
+    >
+      <Image
+        src="/eu.png"
+        alt="yuri"
+        width={420}
+        height={420}
+        className="max-w-[95%] lg:max-w-none"
+      />
+    </div>
+  )
+}
